65311de82ad2e1177d7bc0cfde35647628038f38
Vitogiu <vitogiu1@outlook.com>
2022-03-01 18:53:44 -0300

    🐛

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg button_esmerald"
-            to="/docs/intro">
+            to="docs/general/intro">
             Get Started
           </Link>
         </div>
